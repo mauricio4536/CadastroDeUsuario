f97442dc0f5f776ebbd9c1b6e5fd7cf995efb32e
Gleisson Carillo de Souza <mailto:devecommerce3@belenus.com.br>
2022-08-30 11:40:20 -0300

    EstilizacaoCadastro feat: Foi adicionado novos arquivos jquery de mascara e validação de inputs.

--- js/cadastro.js
+++ js/cadastro.js
@@ -1,2 +1,58 @@
-window.alert('Minha primeira mensagem')
-window.confirm('este site ultiliza coockies voce aceita ?')
+//window.alert('Minha primeira mensagem')
+//window.confirm('este site ultiliza coockies voce aceita ?')
+
+function toSave(url) {
+    if (IsvalidForm()) $("#passenger-form").submit();
+}
+
+function IsvalidForm() {
+    var valid = true;
+
+    if ($("#cpf").val() == "") {
+        $(".valid-cpf").html("CPF é requerido.");
+        valid = false;
+    }
+
+    if ($("#nome").val() == "") {
+        $(".valid-nome").html("Nome é requerido.");
+        valid = false;
+    }
+
+    if ($("#renda_mensal").val() == "") {
+        $(".valid-renda-mensal").html("Renda mensal é requerido.");
+        valid = false;
+    }
+
+    if ($("#numero_dependentes").val() == "") {
+        $(".valid-numero-dependentes").html("Número de dependentes é requerido.");
+        valid = false;
+    }
+
+    return valid;
+}
+
+$('#cpf').on('keypress', function (event) {
+    if ($('#cpf').val() != "") {
+        $(".valid-cpf").html("");
+    }
+});
+
+$('#nome').on('keypress', function (event) {
+    if ($('#nome').val() != "") {
+        $(".valid-nome").html("");
+    }
+});
+
+$("#renda_mensal").on('keypress', function (event) {
+    if ($("#renda_mensal").val() != "") {
+        $(".valid-renda-mensal").html("");
+    }
+});
+
+$("#numero_dependentes").on('keypress', function (event) {
+    if ($("#numero_dependentes").val() != "") {
+        $(".valid-numero-dependentes").html("");
+    }
+});
+
+$(".mask_cpf").mask("999.999.999-99");
